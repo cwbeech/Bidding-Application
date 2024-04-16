fdbb0d41319e99aaa4943f9d27f9c16521bfc5c9
Aidan Beesley <aidanbeesley@gmail.com>
2024-04-16 10:23:16 -0500

    Hopefully git tracks thsi time.

---
+++ Bid501-Server/Bid501-Server/ServerCommunictionControl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Odbc;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace Bid501_Server
+{
+	public class ServerCommunictionControl : WebSocketBehavior
+	{
+		protected override void OnMessage(OdbcInfoMessageEventArgs e)
+		{
+
+		}
+	}
+}
