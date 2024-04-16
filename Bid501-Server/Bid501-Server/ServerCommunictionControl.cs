be282a8c24433b2e36729a1ad02d508a39217f6a
Aidan Beesley <aidanbeesley@gmail.com>
2024-04-16 10:35:04 -0500

    Now with merge

--- Bid501-Server/Bid501-Server/ServerCommunictionControl.cs
+++ Bid501-Server/Bid501-Server/ServerCommunictionControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Data.Odbc;
 using System.Linq;
@@ -6,14 +7,17 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 using WebSocketSharp;
+using WebSocketSharp.Server;
 
 namespace Bid501_Server
 {
 	public class ServerCommunictionControl : WebSocketBehavior
 	{
-		protected override void OnMessage(OdbcInfoMessageEventArgs e)
+		protected override void OnMessage(MessageEventArgs e)
 		{
-
+			string msg = e.Data;
+			msg = msg + " Received in server.";
+			Sessions.Broadcast(msg);
 		}
 	}
 }
