f5b107f3394f589b36b8873586d3b726ea6937aa
jakeesmith01 <111781011+jakeesmith01@users.noreply.github.com>
2024-04-18 10:03:45 -0500

    Login.cs added to Bid501-Server

---
+++ Bid501-Server/Bid501-Server/Login.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Bid501_Server
+{
+    public class Login : WebSocketBehavior
+    {
+        private ServerCommunictionControl _ctrl;
+
+        public Login(ServerCommunictionControl c)
+        {
+            this._ctrl = c;
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            string[] tokens = e.Data.Split(':');
+            string User = tokens[0];
+            string Password = tokens[1];
+
+            _ctrl.LoginDel();
+        }
+
+        public void sendResponse(bool result)
+        {
+            if (result)
+            {
+                Send("Valid");
+            }
+            else
+            {
+                Send("Invalid");
+            }
+        }
+    }
+}
