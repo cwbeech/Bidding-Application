52e0b485451c64871653ae94b33d525a51d838ab
Aidan Beesley <ajbeesley@win2.cis.ksu.edu>
2024-04-18 10:18:55 -0500

    Added IProduct and IProductDB (second doesn't work)

---
+++ Bid501-Server/Bid501-Shared/IProductDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Shared
+{
+    interface IProductDB
+    {
+        //Dictionary<int, IProduct> activeItems;
+        //don't know if we can include this - Aidan
+    }
+}
