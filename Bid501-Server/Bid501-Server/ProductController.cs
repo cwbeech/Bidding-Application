ad5d67b81af5fad55c9cc713c2c49faff2925ac1
jakeesmith01 <111781011+jakeesmith01@users.noreply.github.com>
2024-04-18 11:32:45 -0500

    Added Product, ProductDatabase, ProductController classes

---
+++ Bid501-Server/Bid501-Server/ProductController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Server
+{
+    public class ProductController
+    {
+        private ProductDatabase pd;
+
+        public ProductController(ProductDatabase p)
+        {
+            this.pd = p;
+        }
+
+        /// <summary>
+        /// Validates a bid recieved from the ServerCommunicationControl
+        /// </summary>
+        /// <param name="userID">the userID of the bidder</param>
+        /// <param name="bid">the bid amount they're attempting</param>
+        /// <param name="p">the product that theyre bidding on</param>
+        /// <returns>true if valid, false if not</returns>
+        public bool ValidateBid(int userID, decimal bid, Product p)
+        {
+            if(userID != p.CurrentBidderID && bid > p.MinBid)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Passes a request to update a bid to the product database
+        /// </summary>
+        /// <param name="userID">the userID of the bidder</param>
+        /// <param name="bid">the bid amount</param>
+        /// <param name="p">the product</param>
+        public void UpdateCurrentBid(int userID, decimal bid, Product p) 
+        {
+            pd.UpdateBid(userID, bid, p);
+        }
+
+        /// <summary>
+        /// Passes a request to add a product to the product database
+        /// </summary>
+        /// <param name="name">name of the product</param>
+        /// <param name="desc">description of the product</param>
+        /// <param name="price">price of the product</param>
+        public void AddProduct(string name, string desc, decimal price)
+        {
+            pd.AddProduct(name, desc, price);
+        }
+
+        /// <summary>
+        /// Passes a request to close a bid to the product database
+        /// </summary>
+        /// <param name="p">The product to close</param>
+        public void BidClosed(Product p)
+        {
+            pd.BidClosed(p);
+        }
+    }
+}
---
+++ Bid501-Server/Bid501-Server/ProductDatabase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501_Server
+{
+    public class ProductDatabase
+    {
+        /// <summary>
+        /// A list of all active products and their ID
+        /// </summary>
+        public Dictionary<Product, int> activeItems;
+
+        /// <summary>
+        /// the nextID to assign to a product (not sure if we need this as of rn 04/18)
+        /// </summary>
+        private int nextID;
+
+        /// <summary>
+        /// Constructor for a ProductDatabase
+        /// </summary>
+        /// <param name="p">A ProductController</param>
+        public ProductDatabase()
+        {
+            activeItems = new Dictionary<Product, int>();
+            nextID = 1;
+        }
+
+        /// <summary>
+        /// Adds a product to the activeItems list
+        /// </summary>
+        /// <param name="p">The product to add</param>
+        public void AddProduct(string name, string desc, decimal price)
+        {
+            Product p = new Product(name, desc, nextID, price);
+            nextID++;
+            activeItems.Add(p, p.ID);
+        }
+
+        /// <summary>
+        /// 'Closes' a bid by removing it from the activeItems list
+        /// </summary>
+        /// <param name="p">The product to close</param>
+        public void BidClosed(Product p)
+        {
+            activeItems.Remove(p);
+        }
+
+        /// <summary>
+        /// Updates a products current bidder id and minimum bid 
+        /// </summary>
+        /// <param name="userID">The ID of the bidder</param>
+        /// <param name="bid">The bidder's bid</param>
+        /// <param name="p">The product that they bid on</param>
+        public void UpdateBid(int userID, decimal bid, Product p)
+        {
+            foreach(var kp in  activeItems)
+            {
+                if(kp.Key == p && bid > p.MinBid)
+                {
+                    kp.Key.CurrentBidderID = userID;
+                    kp.Key.MinBid = bid;
+                }
+            }
+        }
+    }
+}
