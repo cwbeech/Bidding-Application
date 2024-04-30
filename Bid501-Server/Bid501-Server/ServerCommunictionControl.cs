417480cccda3f20bbae73ad05eb878c9be20785c
Aidan Beesley <aidanbeesley@gmail.com>
2024-04-30 15:27:21 -0500

    server communication control should be changed. Grabbing stuff Jake pushed now.

--- Bid501-Server/Bid501-Server/ServerCommunictionControl.cs
+++ Bid501-Server/Bid501-Server/ServerCommunictionControl.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using Newtonsoft.Json;
+using System.Net.Configuration;
 
 namespace Bid501_Server
 {
@@ -36,10 +38,35 @@ namespace Bid501_Server
 
 		protected override void OnMessage(MessageEventArgs e)
 		{
-			string msg = e.Data;
+			/*string msg = e.Data;
 			msg = msg + " Received in server.";
 			//Sessions.Broadcast(msg);
-			Sessions.SendTo(ID, msg);
+			Sessions.SendTo(ID, msg);*/
+
+			//string[] msg = e.Data.Split(':');
+			string blah = JsonConvert.DeserializeObject(e.Data).ToString();
+			string[] msg = blah.Split(':');
+			if (Convert.ToInt32(msg[0]) == 0)
+			{//login attempt from client
+			 //bool toReturn = PlaceBidDel(Convert.ToInt32(msg[0]), Convert.ToDecimal(msg[1]), Convert.ToInt32(msg[2]));
+				int toReturn = LoginDel(msg[1], msg[2]); //NOTE: LoginDel needs to return userid which I don't believe it currently does - Aidan, 4/30
+				string sendString = "0:" + toReturn;
+				sendString = JsonConvert.SerializeObject(sendString);
+				Sessions.SendTo(ID, sendString);
+			}
+			else if (Convert.ToInt32(msg[0]) == 1)
+			{//place bid attempt from client
+				bool bidGood = PlaceBidDel(Convert.ToInt32(msg[1]), Convert.ToDecimal(msg[2]), Convert.ToInt32(msg[3]));
+				if (bidGood)
+				{
+					List<string> sendList = ProdInfoDel();
+					string toSend = JsonConvert.SerializeObject(sendList);
+					Sessions.Broadcast(toSend);
+				}
+
+			}
+			
+
 		}
 	}
 }
