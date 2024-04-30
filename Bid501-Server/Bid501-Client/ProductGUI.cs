445a1e9683d050b83a0ef0231ca80a600f2aa194
Dennis Meyer <dmeyer0282@ksu.edu>
2024-04-30 10:43:34 -0500

    Set up ProductGUI, missing TIME of expiration

--- Bid501-Server/Bid501-Client/ProductGUI.cs
+++ Bid501-Server/Bid501-Client/ProductGUI.cs
@@ -12,23 +12,31 @@ namespace Bid501_Client
 {
     public partial class ProductGUI : Form
     {
-        //make list for products of whatever form
+        ProductDatabaseProxy database = new ProductDatabaseProxy();
+        //NEEDS TO BE SET SOMETIME DURING SETUP
         public ProductGUI()
         {
             InitializeComponent();
-            //Set up listbox
+            uxProductBox.DataSource = database.activeItems;
         }
 
         //When selected index changed, update pretty much everything: ProductName, timeLeft, minBidAmount, NumofBids, match current item selected
         private void uxProductBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            //TWO ITEMS ARE NOT IMPLEMENTED ON THIS LIST
+            uxProductName.Text = ((ProductProxy)uxProductBox.SelectedItem).name;
+            //uxTimeLeft.Text = ((ProductProxy)uxProductBox.SelectedItem).time;
+            uxMinBidAmount.Text = ((ProductProxy)uxProductBox.SelectedItem).minBid.ToString();
+            uxDetail.Text = ((ProductProxy)uxProductBox.SelectedItem).description;
         }
 
         //When clicking to bid, first validate that amount is valid, then try to send bid
         private void uxBidConfirm_Click(object sender, EventArgs e)
         {
-
+            if (int.Parse(uxBidAmount.Text) >= ((ProductProxy)uxProductBox.SelectedItem).minBid)
+            {
+                //sEND bID
+            }
         }
     }
 }
