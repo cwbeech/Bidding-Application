9a868fafe7f584a603c509a9390a8793433fdae4
jakeesmith01 <jakeesmith@ksu.edu>
2024-05-04 16:39:04 -0500

    fixed pls

--- Bid501-Server/Bid501-Client/ProductGUI.cs
+++ Bid501-Server/Bid501-Client/ProductGUI.cs
@@ -68,16 +68,18 @@ namespace Bid501_Client
                         if (!pNew.Contains(p))
                         {
                             prods.Remove((ProductProxy)p);
+
+                            if (p.currBidID == clientID)
+                            {
+                                MessageBox.Show("You won!");
+                            }
+                            else
+                            {
+                                MessageBox.Show("You lost");
+                            }
                         }
 
-                        if(p.currBidID == clientID)
-                        {
-                            MessageBox.Show("You won!");
-                        }
-                        else
-                        {
-                            MessageBox.Show("You lost");
-                        }
+                       
                     }
 
                     foreach (IProduct p in pNew)
