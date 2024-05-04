c7f296e8fd012946a593655bbaf82738a078201b
Dennis Meyer <dmeyer0282@ksu.edu>
2024-05-04 14:37:54 -0500

    qol

--- Bid501-Server/Bid501-Client/ProductGUI.Designer.cs
+++ Bid501-Server/Bid501-Client/ProductGUI.Designer.cs
@@ -158,6 +158,7 @@
             // 
             // ProductGUI
             // 
+            this.AcceptButton = this.uxBidConfirm;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(406, 264);
